d0d470a70f2aadaef7bf8712f90bb78a2c3e6f40
Matteo Falasca <matteo.falasca@emmek.org>
2023-10-11 15:24:39 +0200

    Es2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import Shop.Product;
 import functional_interfaces.ProductsFilter;
 
+import java.util.List;
 import java.util.function.Predicate;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
@@ -11,7 +12,8 @@ public class Main {
         Predicate<Product> isOverTon = product -> product.getPrice() > 100;
         ProductsFilter booksOverTon = products -> products.stream().filter(product -> isBook.test(product) && isOverTon.test(product)).toList();
 
-
+        Predicate<Product> isBaby = product -> product.getCategory().equals("Baby");
+        Predicate<List<Product>> hasBaby = products -> products.stream().anyMatch(isBaby);
     }
 
 
