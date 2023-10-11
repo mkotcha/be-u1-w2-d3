c5fb0ab5bc066fdbc35001a087e5fbd9323efdff
Matteo Falasca <matteo.falasca@emmek.org>
2023-10-11 15:08:24 +0200

    Es1 with Predicate

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
-import functional_interfaces.ProductFilter;
+import Shop.Product;
+import functional_interfaces.ProductsFilter;
+
+import java.util.function.Predicate;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-
-        ProductFilter booksOverTon = products -> products.stream().filter(product -> product.getCategory().equals("Books") && product.getPrice() > 100).toList();
+        Predicate<Product> isBook = product -> product.getCategory().equals("Books");
+        Predicate<Product> isOverTon = product -> product.getPrice() > 100;
+        ProductsFilter booksOverTon = products -> products.stream().filter(product -> isBook.test(product) && isOverTon.test(product)).toList();
 
 
     }
--- src/functional_interfaces/ProductsFilter.java
+++ src/functional_interfaces/ProductsFilter.java
@@ -5,6 +5,6 @@ import Shop.Product;
 import java.util.List;
 
 @FunctionalInterface
-public interface ProductFilter {
+public interface ProductsFilter {
     public List<Product> getProducts(List<Product> products);
 }
